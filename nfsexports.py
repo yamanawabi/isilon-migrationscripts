64d7605385fa43bd11187fff176eed14491e5616
Yama Nawabi <mknawabi@gmail.com>
2021-03-07 09:45:53 -0800

    remove unnecessary if block

--- nfsexports.py
+++ nfsexports.py
@@ -15,84 +15,83 @@ with open('depot_nfs_exports.json') as nfsexports:
 
 
 for i in nfs_export_data:
-    if "paths" in i:
 
-        # comma separated client list
+    # comma separated client list
+    clients = ""
+    if (len(i['clients']) == 0):
+        # no clients listed
+        pass
+    else:
+        for client in i['clients']:
+            clients += client + ","
+    # remove trailing comma
+    if(clients):
+        clients = " --clients " + clients[0:len(clients)-1]
+    else: 
         clients = ""
-        if (len(i['clients']) == 0):
-            # no clients listed
-            pass
-        else:
-            for client in i['clients']:
-                clients += client + ","
-        # remove trailing comma
-        if(clients):
-            clients = " --clients " + clients[0:len(clients)-1]
-        else: 
-            clients = ""
 
-        # comma separated root client list
+    # comma separated root client list
+    rootclients = ""
+    if (len(i['root_clients']) == 0):
+        # no clients
+        pass
+    else:
+        for rootclient in i['root_clients']:
+            rootclients += rootclient + ","
+    # remove trailing comma
+    if(rootclients): 
+        rootclients = " --root-clients " + rootclients[0:len(rootclients)-1]
+    else:
         rootclients = ""
-        if (len(i['root_clients']) == 0):
-            # no clients
-            pass
-        else:
-            for rootclient in i['root_clients']:
-                rootclients += rootclient + ","
-        # remove trailing comma
-        if(rootclients): 
-            rootclients = " --root-clients " + rootclients[0:len(rootclients)-1]
-        else:
-            rootclients = ""
-        
-        # comma separated read-write client list
+    
+    # comma separated read-write client list
+    rwclients = ""
+    if (len(i['read_write_clients']) == 0):
+        # no read-write clients
+        pass
+    else:
+        for rwclient in i['read_write_clients']:
+            rwclients += rwclient + ","
+    # remove trailing comma
+    if(rwclients): 
+        rwclients = " --read-write-clients " + rwclients[0:len(rwclients)-1]
+    else:
         rwclients = ""
-        if (len(i['read_write_clients']) == 0):
-            # no read-write clients
-            pass
-        else:
-            for rwclient in i['read_write_clients']:
-                rwclients += rwclient + ","
-        # remove trailing comma
-        if(rwclients): 
-            rwclients = " --read-write-clients " + rwclients[0:len(rwclients)-1]
-        else:
-            rwclients = ""
 
-        # comma separated read-only client list
+    # comma separated read-only client list
+    roclients = ""
+    if (len(i['read_only_clients']) == 0):
+        # no read-only clients
+        pass
+    else:
+        for roclient in i['read_only_clients']:
+            roclients += roclient + ","
+    # remove trailing comma
+    if(roclients): 
+        roclients = " --read-only-clients " + roclients[0:len(roclients)-1]
+    else:
         roclients = ""
-        if (len(i['read_only_clients']) == 0):
-            # no read-only clients
-            pass
-        else:
-            for roclient in i['read_only_clients']:
-                roclients += roclient + ","
-        # remove trailing comma
-        if(roclients): 
-            roclients = " --read-only-clients " + roclients[0:len(roclients)-1]
-        else:
-            roclients = ""
 
-        # read only mount
-        readonly = ""
-        if (i['read_only'] == False):
-            pass
-        else:
-            readonly = "--read-only yes"
+    # read only mount
+    readonly = ""
+    if (i['read_only'] == False):
+        pass
+    else:
+        readonly = "--read-only yes"
 
-        # export description
-        description = ""
-        if str(i['description']) == "":
-            # no description data
-            pass
-        else:
-            description = " --description \""+ str(i['description']) + "\""
+    # export description
+    description = ""
+    if str(i['description']) == "":
+        # no description data
+        pass
+    else:
+        description = " --description \""+ str(i['description']) + "\""
 
-        # put together the command per nfs export
-        command = "isi nfs exports create "
-        path = str(i['paths'][0])
-        
-        print(command + path + clients + rootclients + roclients + rwclients +
-                description + readonly + "\n")
+    # put together the command per nfs export
+    command = "isi nfs exports create "
+    path = str(i['paths'][0])
+    
+    print(command + path + clients + rootclients + roclients + rwclients +
+            description + readonly + "\n")
 
 
